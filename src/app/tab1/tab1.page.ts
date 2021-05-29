62dd0e065229a47c57e3a92fc3d921d3208cf9f2
Renan Aleixo Paganatto <renan.paganatto@etec.sp.gov.br>
2021-05-29 11:45:46 -0300

    Adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from './../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -5,11 +6,63 @@ import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  titulo = 'Videos';
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  listaFilmes: IFilme[] = [
+    {
+      nome: 'Rogai por Nós (2021)',
+      lancamento: '02/04/2021 (US)',
+      duracao: '1h 39m',
+      classificacao: 73,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zduyzcjyahZtP4fGZE6789h04IM.jpg',
+      generos: ['Terror']
+    },
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021 (BR)',
+      duracao: '1h 50m',
+      classificacao: 71,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome: 'Godzilla vs. Kong (2021)',
+      lancamento: '01/04/2021 (BR)',
+      duracao: '1h 53m',
+      classificacao: 81,
+      cartaz:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wllzjZxg4ynlAm5xmOICJ2uHOPJ.jpg",
+      generos: ['Ação', 'Ficção científica']
+    },
+    {
+      nome: 'Aqueles Que Me Desejam a Morte (2021)',
+      lancamento: '14/05/2021 (US)',
+      duracao: '1h 40m',
+      classificacao: 71,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qNi8uFKHAN5qt9UU5PpDi8cjWj1.jpg',
+      generos: ['Thriller', 'Drama', 'Ação', 'Mistério'],
+    },
+    {
+      nome: 'Sem Remorso (2021)',
+      lancamento: '30/04/2021 (BR)',
+      duracao: '1h 50m',
+      classificacao: 72,
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos: ['Ação', 'Thriller', 'Guerra'],
+    }
+  ];
+
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -21,14 +74,15 @@ export class Tab1Page {
           role: 'cancel',
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
-          }
-        }, {
+          },
+        },
+        {
           text: 'Sim, favoritar!',
           handler: () => {
             this.apresentarToast();
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -38,9 +92,8 @@ export class Tab1Page {
     const toast = await this.toastController.create({
       message: 'Filme adicionado aos favoritos.',
       duration: 2000,
-      color: "success"
+      color: 'success',
     });
     toast.present();
   }
-
 }
